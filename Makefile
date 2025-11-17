700e5038991a0a86b7c7774462699961eb694625
Douae Abrouri <doabrour@c2r11p2.1337.ma>
2025-11-17 15:23:05 +0100

    ...

--- Makefile
+++ Makefile
@@ -6,21 +6,10 @@
 #    By: doabrour <doabrour@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/11/14 15:48:53 by doabrour          #+#    #+#              #
-#    Updated: 2025/11/16 19:19:54 by doabrour         ###   ########.fr        #
+#    Updated: 2025/11/17 14:42:13 by doabrour         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-RED     := \033[1;31m
-YELLOW  := \033[1;33m
-GREEN   := \033[1;32m
-CYAN    := \033[1;36m
-BLUE    := \033[1;34m
-MAGENTA := \033[1;35m
-RESET   := \033[0m
-
-define RAINBOW
-$(RED)$(1)$(YELLOW)$(2)$(GREEN)$(3)$(CYAN)$(4)$(BLUE)$(5)$(MAGENTA)$(6)$(RESET)
-endef
 
 NAME := libftprintf.a
 
@@ -33,16 +22,15 @@ LIB := ar rcs
 OBG := $(src:.c=.o)
 
 all : $(NAME)
-	 @echo "$(call RAINBOW,everything ,is ,reday ,bro... ,ft_printf)"
 
 $(NAME): $(OBG)
 	@$(LIB) $(NAME) $?
 
 test: main.c
-	@cc $(CFLAGS) main.c $(NAME) -o test 
+	@$(CC) $(CFLAGS) main.c $(NAME) -o test 
 
 %: %.o main.c 
-	@cc $(CFLAGS) $^ -o $@
+	@$(CC) $(CFLAGS) $^ -o $@
 
 %.o : %.c ft_printf.h
 	@$(CC) $(CFLAGS) -c $< -o $@
@@ -62,4 +50,4 @@ tclean: fclean
 re:
 	clean all
 
-.PHONY: clean
+.PHONY: clean re all fclean tclean
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: doabrour <doabrour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 14:16:59 by douaeoxo          #+#    #+#             */
-/*   Updated: 2025/11/16 19:08:36 by doabrour         ###   ########.fr       */
+/*   Updated: 2025/11/17 10:38:29 by doabrour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,25 @@
 
 static	int	compare(char c, va_list list)
 {
-	int		w;
-
-	w = 0;
 	if (c == 's')
-		w = print_string(va_arg(list, char *));
+		return (print_string(va_arg(list, char *)));
 	else if (c == 'u')
-		w = print_unsigned(va_arg(list, unsigned int));
+		return (print_unsigned(va_arg(list, unsigned int)));
 	else if (c == 'd')
-		w = print_decimal(va_arg(list, int));
+		return (print_decimal(va_arg(list, int)));
 	else if (c == 'i')
-		w = print_integer(va_arg(list, int));
+		return (print_integer(va_arg(list, int)));
 	else if (c == 'x')
-		w = print_lower_hexadecimal(va_arg(list, unsigned int));
+		return (print_lower_hexadecimal(va_arg(list, unsigned int)));
 	else if (c == 'X')
-		w = print_upper_hexadecimal(va_arg(list, unsigned int));
+		return (print_upper_hexadecimal(va_arg(list, unsigned int)));
 	else if (c == 'p')
-		w = print_pointer(va_arg(list, void *));
+		return (print_pointer(va_arg(list, void *)));
 	else if (c == 'c')
-		w = print_character(va_arg(list, int));
+		return (print_character(va_arg(list, int)));
 	else if (c == '%')
-		w = write(1, "%%", 1);
-	if (w == -1)
-		return (-1);
-	return (w);
+		return (write(1, "%%", 1));
+	return (0);
 }
 
 int	ft_printf(const char *str, ...)
